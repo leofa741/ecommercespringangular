ef99128f41b148ecc3bc01fef0766761c35b37e0
leo <leonardofarena@gmail.com>
2023-05-02 14:23:19 -0300

    nn

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oa98xj1g44VNbMwe5d7',
         issuer: 'https://dev-60880607.okta.com/oauth2/default',
-        redirectUri: 'https://ecommercespringback.vercel.app/login/callback',
+        redirectUri: 'https://ecommercespringangular.vercel.app/login/callback',
         scopes: ['openid', 'profile', 'email'],
         // pkce: true
 
